557e8d72834b9b2bb802efdab26e196983301015
Mulax Prime <161124347+romeobwiii@users.noreply.github.com>
2025-09-01 12:38:03 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.scss';
 import { Provider } from 'react-redux';
-import { store } from './store/store.jsx';
+import { store } from './store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
+
